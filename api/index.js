de2976cb16d7dfccfd6c07371946a7fdc0f0520e
Aleksei Falchuk <sddw28@gmail.com>
2023-10-05 15:46:35 +0200

    feat: add signup api route

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRouter from "./routes/user.route.js"
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -16,13 +17,15 @@ mongoose
 
 const app = express();
 
+app.use(express.json())
+
 app.listen(3000, () => {
   console.log("Server port 3000  running");
 });
-
 
 // app.get('/test', (req, res) => {
 //   res.send('Hello res.send')
 // });
 
-app.use("/api/user", userRouter)
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
