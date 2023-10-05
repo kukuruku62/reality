a0d3b6e9b197e21a139ac79a64ac6365f74d5211
Aleksei Falchuk <sddw28@gmail.com>
2023-10-05 16:07:17 +0200

    feat: add middleware and func to handle possible error

--- api/index.js
+++ api/index.js
@@ -17,7 +17,7 @@ mongoose
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log("Server port 3000  running");
@@ -29,3 +29,13 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal server error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
